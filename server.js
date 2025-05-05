f6d395fa2547d4bbebe68a008bf8cb7c4e65805b
nataliakif <nataliakif@gmail.com>
2025-05-05 21:45:58 +0200

    mongo update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const app = require("./app");
 
 const { DB_HOST, PORT } = process.env;
-
+mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
   .then(() =>
